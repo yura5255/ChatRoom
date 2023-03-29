9e2c57e850792a120a013c5743a8e48e5bb3d2c6
Yura <Yura@DESKTOP-QP2HQT0>
2023-03-29 21:47:41 +0300

    Add project files.

---
+++ ClientApp/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IPEndPoint serverEndPoint;
+        //const string serverAddress = "127.0.0.1";
+        //const short serverPort = 4040;
+        UdpClient client = new UdpClient();
+        ObservableCollection<MessageInfo> messages = new ObservableCollection<MessageInfo>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = messages;
+            string serverAddress = ConfigurationManager.AppSettings["ServerAddress"]!;
+            short serverPort = short.Parse(ConfigurationManager.AppSettings["ServerPort"]!);
+            serverEndPoint = new IPEndPoint(IPAddress.Parse(serverAddress), serverPort);
+        }
+
+        private async void SendBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string message = msgTextBox.Text;
+            SendMessage(message);
+        }
+
+        private void JoinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "$<join>";
+            SendMessage(message);
+            Listen();
+        }
+        private async void SendMessage(string message)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            await client.SendAsync(data, data.Length, serverEndPoint);
+        }
+        private async void Listen()
+        {
+            while (true)
+            {
+                var res = await client.ReceiveAsync();
+                string message = Encoding.Unicode.GetString(res.Buffer);
+                messages.Add(new MessageInfo(message));
+            }
+        }
+    }
+    class MessageInfo
+    {
+        public string Message { get; set; }
+        public DateTime Time { get; set; }
+        public MessageInfo(string message)
+        {
+            Message = message;
+            Time = DateTime.Now;
+        }
+        public override string ToString()
+        {
+            return $" {Message} : {Time.ToShortDateString()} ";
+        }
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+ChatServer server = new ChatServer();
+server.Start();
+class ChatServer
+{
+    const short port = 4040;
+    const string JOIN_CMD = "$<join>";
+    UdpClient server = new UdpClient(port);
+    HashSet<IPEndPoint> members = new HashSet<IPEndPoint>();
+    IPEndPoint clientEndPoint = null;
+    void AddMember(IPEndPoint member)
+    {
+        members.Add(member);
+        Console.WriteLine("Member was added!");
+
+    }
+    private void SendToAll(byte[] data)
+    {
+        foreach (IPEndPoint member in members)
+        {
+            server.SendAsync(data, data.Length, member);
+        }
+    }
+    public void Start()
+    {
+        while (true)
+        {
+            byte[] data = server.Receive(ref clientEndPoint);
+            string message = Encoding.Unicode.GetString(data);
+            Console.WriteLine($"{message} at {DateTime.Now.ToShortTimeString()}" + $" from {clientEndPoint}");
+            if (message == JOIN_CMD)
+            {
+                AddMember(clientEndPoint);
+            }
+            else
+            {
+                SendToAll(data);
+            }
+        }
+
+    }
+}
